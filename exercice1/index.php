e651a1711e6da28d1aaa5a10c146d856f812d2aa
Mathilde <mathilde.nadeau@hotmail.fr>
2021-11-04 14:50:43 +0100

    correction erreur

--- exercice1/index.php
+++ exercice1/index.php
@@ -3,6 +3,6 @@ $valeurBA=12;
 $valeurAC=8;
 $valeurBC=undefined;
 function hypothenuse ($valeurBA, $valeurAC){
-    $valeurBC =((pow($valeurBA,2))+(pow($valeurAC,2)))
+    $valeurBC =(sqrt(float(pow($valeurBA,2))+(pow($valeurAC,2))): float)
 };
 ?>
